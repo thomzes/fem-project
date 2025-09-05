6e15a6140c06ce4348b0c587e5024589d4c5c413
thomzes <thomz161@gmail.com>
2025-09-05 21:42:21 +0700

    add

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,18 +1,41 @@
 package app
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/thomzes/fem-project/internal/api"
+	"github.com/thomzes/fem-project/internal/store"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	app := &Application{Logger: logger}
+	workoutHandler := api.NewWorkoutHandler()
+
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
+	}
 
 	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this is health check func!")
 }
--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/thomzes/fem-project/internal/app"
+	"github.com/thomzes/fem-project/internal/routes"
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
+	defer app.DB.Close()
 
-	app.Logger.Println("we are running our app")
+	app.Logger.Printf("we are running on port %d\n", port)
 
-	http.HandleFunc("/health", HealthCheck)
+	r := routes.SetupRoutes(app)
 
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
@@ -29,8 +37,4 @@ func main() {
 	if err != nil {
 		app.Logger.Panic(err)
 	}
-}
-
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is health check func!")
 }
